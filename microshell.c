938bccd0dc256cb9e65ed34d9f10d796fa20ee71
Pedro Palomares <pedpalgarob@gmail.com>
2024-10-03 13:20:46 +0200

    progress

--- microshell.c
+++ microshell.c
@@ -6,59 +6,25 @@
 
 #include <stdio.h>
 
-void	execute(char **args)
+char	**take_command(char **argv)
 {
-	while (*args)
-	{
-		write(1, *args, strlen(*args));
-		write(1, " - ", 3);
-		args++;
-	}
-}
 
-char	***parse(char **argv)
-{
-	char	***parsed_args;
-
-	int		sc_n = 1;
-	int		i = 1;
-	while (argv[i])
+	int	i;
+	for (i = 1; *argv[i] != ';'; i++)
 	{
-		if (*argv[i] == ';' && argv[i + 1] != 0)
-			sc_n++;
-		i++;
-	}
-	parsed_args = (char ***)malloc(sizeof(char **) * (sc_n + 1));
-
-	int j = 0;
-	i = 1;
-	while (argv[i])
-	{
-		while (*argv[i] != ';')
+		if (*argv[i] != '|')
 		{
-			parsed_args[j] = &argv[i];
+			char	**split = ft_split(argv[i])
 		}
-		i++;
+		else
+
 	}
-	printf("%p\n", argv[i]);
-	parsed_args[j] = 0;
-	return (parsed_args);
 }
 
 int	main(int argc, char **argv)
 {
-	char	***parsed_args;
-	int		i = 1;
-
-	if (argc > 1)
-	{
-		parsed_args = parse(argv); // Esto debería separar según ;
-		while (parsed_args[i])
-		{
-			execute(parsed_args[i]);
-			write(1, "\n", 1);
-			i++;
-		}
-	}
+	argv++;
+	while (argv)
+		argv = take_command(argv); // Esto devuelve lo que haya justo después de ';' o un puntero nulo si ya ha dejado de leer
 	return(0);
 }
