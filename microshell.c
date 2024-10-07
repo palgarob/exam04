e64e66e8a3a20886806ddde8bc31654c0094309b
Pedro Palomares Garcia-Obregon <pepaloma@c3r3s5.42urduliz.com>
2024-10-07 15:07:25 +0200

    progress

--- microshell.c
+++ microshell.c
@@ -15,23 +15,34 @@ int	there_are_pipes(char **argv)
 	return (-i);
 }
 
+size_t	ft_strlen(char *str)
+{
+	size_t i = 0;
+
+	while (str[i]) i++;
+	return (i);
+}
+
 char	**exec_wo_pipe(int *last_pipe, char **argv, char **envp)
 {
-	if (last_pipe)
+	pid_t	pid; pid = fork(); if (pid < 0) {write(STDERR_FILENO, "error: fatal\n", 13); exit(1);}
+	if (!pid)
 	{
-		pid_t	pid; pid = fork(); if (pid < 0) {write(STDERR_FILENO, "error: fatal\n", 13); exit(1);}
-		if (!pid)
+		if (last_pipe)
 		{
 			if (dup2(last_pipe[0], STDIN_FILENO) < 0) {write(STDERR_FILENO, "error: fatal\n", 13); exit(1);}
 			close(last_pipe[1]);
 			close(last_pipe[0]);
-			argv[-there_are_pipes(argv)] = NULL;
-			if (execve(*argv, argv, envp)) {write(STDERR_FILENO, "error: cannot execute ", 22), write(STDERR_FILENO, *argv, ft_strlen(*argv)); write(STDERR_FILENO, "\n", 1);}
 		}
-		close(last_pipe[0]);
-		close(last_pipe[1]);
+		argv[-there_are_pipes(argv)] = NULL;
+		if (execve(*argv, argv, envp)) {write(STDERR_FILENO, "error: cannot execute ", 23), write(STDERR_FILENO, *argv, ft_strlen(*argv)); write(STDERR_FILENO, "\n", 1);}
 	}
+	if (last_pipe) {close(last_pipe[1]); close(last_pipe[0]);}
+	waitpid(pid, NULL, 0);
+	if (argv[-there_are_pipes(argv)])
+		return (&argv[-there_are_pipes(argv) + 1]);
 	else
+		return (&argv[-there_are_pipes(argv)]);
 }
 
 char	**exec_with_pipe(int *last_pipe, char **argv, char **envp)
@@ -54,15 +65,31 @@ char	**exec_with_pipe(int *last_pipe, char **argv, char **envp)
 		if (execve(*argv, argv, envp)) {write(STDERR_FILENO, "error: cannot execute ", 22), write(STDERR_FILENO, *argv, ft_strlen(*argv)); write(STDERR_FILENO, "\n", 1);}
 	}
 	if (last_pipe) {close(last_pipe[1]); close(last_pipe[0]);}
-	if (there_are_pipes(argv[there_are_pipes(argv) + 1]) > 0)
+	if (there_are_pipes(&argv[there_are_pipes(argv) + 1]) > 0)
 		return (exec_with_pipe(pipex, &argv[there_are_pipes(argv) + 1], envp));
 	else
 		return (exec_wo_pipe(pipex, &argv[there_are_pipes(argv) + 1], envp));
 	
 }
 
+char	**exec_builtin(char **argv)
+{
+	if (there_are_pipes(argv) != -2) write(STDERR_FILENO, "error: cd: bad arguments\n", 25);
+	if (chdir(argv[1]))
+	{
+		write(STDERR_FILENO, "error: cd: cannot change directory to ", 38);
+		write(STDERR_FILENO, argv[1], ft_strlen(argv[1]));
+		write(STDERR_FILENO, "\n", 1);
+	}
+	if (argv[-there_are_pipes(argv)])
+		return (&argv[-there_are_pipes(argv) + 1]);
+	else
+		return (&argv[-there_are_pipes(argv)]);
+}
+
 char	**take_command(char **argv, char **envp)
 {
+	if (!strcmp("cd", argv[0])) return (exec_builtin(argv));
 	if (there_are_pipes(argv) > 0) return (exec_with_pipe(NULL, argv, envp));
 	else return (exec_wo_pipe(NULL, argv, envp));
 }
