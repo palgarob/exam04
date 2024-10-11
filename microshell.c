9e95b1898bfaa1027000adb7e4b8129ba24b9f1a
Pedro Palomares Garcia-Obregon <pepaloma@c3r6s6.42urduliz.com>
2024-10-11 12:48:31 +0200

    There are two versions: This one AND the previous commit. I have to tidy this up someway

--- microshell.c
+++ microshell.c
@@ -1,101 +1,87 @@
-#include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
 #include <string.h>
 #include <sys/wait.h>
-
-#include <errno.h>
-#include <stdio.h>
+#include <stdlib.h>
 
 static int return_value;
 
-int	find_pipe(char **argv)
+int	check_pipe(char **argv)
 {
-	int i;
-
-	for (i = 0; argv[i] && strcmp(";", argv[i]); i++)
-		if (!strcmp("|", argv[i])) return (i);
+	int	i;
+	for (i = 0; argv[i] && strcmp(argv[i], ";"); i++) if (strcmp(*argv, "|")) return (i);
 	return (-i);
 }
 
-void	print_err(char *str)
+void	p_error(char *str)
 {
-	size_t i = 0;
-
+	size_t	i = 0;
 	while (str[i]) i++;
 	write(STDERR_FILENO, str, i);
 }
 
-char	**exec_last(int *last_pipe, char **argv, char **envp)
-{
-	pid_t	pid; pid = fork(); if (pid < 0) {print_err("error: fatal\n"); exit(1);}
-	if (!pid)
-	{
-		if (last_pipe)
-		{
-			if (dup2(last_pipe[0], STDIN_FILENO) < 0) {print_err("erxror: fatal\n"); exit(1);}
-			close(last_pipe[1]);
-			close(last_pipe[0]);
-		}
-		argv[-find_pipe(argv)] = NULL;
-		if (execve(*argv, argv, envp)) {print_err("error: cannot execute "); print_err(*argv); print_err("\n"); exit(1);}
-	}
-	if (last_pipe) {close(last_pipe[1]); close(last_pipe[0]);}
-	waitpid(pid, &return_value, 0);
-	return (&argv[-find_pipe(argv)]);
-}
-
-char	**exec_pipe(int *last_pipe, char **argv, char **envp)
-{
-	int	pipex[2]; if (pipe(pipex) < 0) {print_err("error: fatal\n"); exit(1);}
-
-	pid_t	pid; pid = fork(); if (pid < 0) {print_err("error: fatal\n"); exit(1);}
-	if (!pid)
-	{
-		if (last_pipe)
-		{
-			if (dup2(last_pipe[0], STDIN_FILENO) < 0) {print_err("error: fatal\n"); exit(1);}
-			close(last_pipe[0]);
-			close(last_pipe[1]);
-		}
-		if (dup2(pipex[1], STDOUT_FILENO) < 0) {print_err("error: fatal\n"); exit(1);}
-		close(pipex[1]); close(pipex[0]);
-		argv[find_pipe(argv)] = NULL;
-		if (execve(*argv, argv, envp)) {print_err("error: cannot execute "), print_err(*argv); print_err("\n"); exit(1);}
-	}
-	if (last_pipe) {close(last_pipe[1]); close(last_pipe[0]);}
-	if (find_pipe(&argv[find_pipe(argv) + 1]) > 0)
-		return (exec_pipe(pipex, &argv[find_pipe(argv) + 1], envp));
-	else
-		return (exec_last(pipex, &argv[find_pipe(argv) + 1], envp));
-	
-}
-
 char	**exec_builtin(char **argv)
 {
-	if (find_pipe(argv) != -2) {print_err("error: cd: bad arguments\n"); return (&argv[-find_pipe(argv)]);}
-	if (chdir(argv[1])) {print_err("error: cd: cannot change directory to "); print_err(argv[1]); print_err("\n");}
-	return (&argv[-find_pipe(argv)]);
+	if (check_pipe(argv) != -2) {p_error("error: cd: bad arguments\n"); return (argv + -check_pipe(argv));}
+	if (chdir(argv[1])) {p_error("error: cd: cannot change directory to "); p_error(argv[1]); p_error("\n");}
+	return (argv + 2);	
 }
 
-char	**take_command(char **argv, char **envp)
+char	**exec_last(char **argv, char** envp, int *last_pipe)
 {
-	if (!strcmp("cd", argv[0])) return (exec_builtin(argv));
-	if (find_pipe(argv) > 0) return (exec_pipe(NULL, argv, envp));
-	else return (exec_last(NULL, argv, envp));
+	pid_t	pid = fork(); if (pid < 0) {p_error("error: fatal\n"); exit(1);}
+
+	if (!pid)
+	{
+		if (last_pipe)
+		{
+			if (dup2(last_pipe[0], STDIN_FILENO) < 0) {p_error("error: fatal\n"); exit(1);}
+			close(last_pipe[0]); close(last_pipe[1]);
+		}
+		argv[-check_pipe(argv)] = NULL;
+		if (execve(*argv, argv, envp)) {p_error("error: cannot execute "); p_error(*argv); p_error("\n"); exit(1);}
+	}
+	if (last_pipe) {close(last_pipe[0]); close(last_pipe[1]);}
+	waitpid(pid, &return_value, 0);
+	return (argv + -check_pipe(argv));
+}
+
+char	**exec_pipe(char **argv, char **envp, int *last_pipe)
+{
+	int	pipex[2]; if (pipe(pipex) < 0) {p_error("error: fatal\n"); exit(1);}
+	pid_t	pid = fork(); if (pid < 0) {p_error("error: fatal\n"); exit(1);}
+	if (!pid)
+	{
+		if (last_pipe)
+		{
+			if (dup2(last_pipe[0], STDIN_FILENO) < 0) {p_error("error: fatal\n"); exit(1);}
+			close(last_pipe[0]); close(last_pipe[1]);
+		}
+		if (dup2(pipex[1], STDOUT_FILENO) < 0) {p_error("error: fatal\n"), exit(1);}
+		argv[check_pipe(argv)] = NULL;
+		close(pipex[0]); close(pipex[1]);
+		if (execve(*argv, argv, envp)) {p_error("error: cannot execute "); p_error(*argv); p_error("\n"); exit(1);}
+	}
+	if (last_pipe) {close(last_pipe[0]); close(last_pipe[1]);}
+	if (check_pipe(argv + check_pipe(argv) + 1) > 0) return (exec_pipe(argv + check_pipe(argv) + 1, envp, pipex));
+	else return (exec_last(argv + check_pipe(argv) + 1, envp, pipex));
+}
+
+char	**enter(char **argv, char ** envp)
+{
+	if (!strcmp(*argv, "cd")) return (exec_builtin(argv));
+	if (check_pipe(argv) > 0) return (exec_pipe(argv, envp, NULL));
+	else return (exec_last(argv, envp, NULL));
 }
 
 int	main(__attribute__((unused)) int argc, char **argv, char **envp)
-{ 
+{
 	argv++;
 	while (*argv)
 	{
-		while (!strcmp(";", *argv)) {
-			argv++;
-			if (!*argv) break;
-		}
+		while (!strcmp(*argv, ";")) {argv++; if (!*argv) break;}
 		if (!*argv) break;
-		argv = take_command(argv, envp);
+		argv = enter(argv, envp);
 	}
 	return (return_value);
 }
+
